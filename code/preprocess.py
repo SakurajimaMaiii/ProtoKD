a296c1ab90e00fe78a434976230495f0510973d4
wangs <bit.ybws@gmail.com>
2026-02-21 20:23:42 +0800

    refactor

--- code/preprocess.py
+++ code/preprocess.py
@@ -3,6 +3,7 @@ preprocess data (nii.gz) to npy
 """
 
 import os
+import argparse
 import SimpleITK as sitk
 import numpy as np
 
@@ -29,36 +30,60 @@ def zscore_nonzero(img):
     return img
 
 
-path = "../../Dataset/BraTS2018/brats2018"  # change this if need
-outputs = "../data/brats2018/"
-if not os.path.exists(outputs):
-    os.makedirs(outputs)
-for ii in ["HGG", "LGG"]:
-    path1 = os.path.join(path, ii)
-    all_subjects = os.listdir(path1)
-    for sub in all_subjects:
-        sub_path = os.path.join(path1, sub)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Preprocess BraTS2018 dataset')
+    parser.add_argument('--input_path', type=str, 
+                       default='../../Dataset/BraTS2018/brats2018',
+                       help='Input path to BraTS2018 dataset')
+    parser.add_argument('--output_path', type=str,
+                       default='../data/brats2018/',
+                       help='Output path for processed npy files')
+    return parser.parse_args()
 
-        t1_path = sub_path + "/" + sub + "_t1.nii.gz"
-        t2_path = sub_path + "/" + sub + "_t2.nii.gz"
-        t1ce_path = sub_path + "/" + sub + "_t1ce.nii.gz"
-        flair_path = sub_path + "/" + sub + "_flair.nii.gz"
-        seg_path = sub_path + "/" + sub + "_seg.nii.gz"
 
-        t1_img = read_nii(t1_path)
-        t2_img = read_nii(t2_path)
-        t1ce_img = read_nii(t1ce_path)
-        flair_img = read_nii(flair_path)
-        seg = read_nii(seg_path)
-        seg = seg.astype(np.int8)
+def main():
+    args = parse_args()
+    
+    input_path = args.input_path
+    output_path = args.output_path
+    
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        print(f"Created output directory: {output_path}")
+    
+    for tumor_type in ["HGG", "LGG"]:
+        type_path = os.path.join(input_path, tumor_type)
+        all_subjects = os.listdir(type_path)
+        
+        for sub in all_subjects:
+            sub_path = os.path.join(type_path, sub)
+            
+            t1_path = os.path.join(sub_path, f"{sub}_t1.nii.gz")
+            t2_path = os.path.join(sub_path, f"{sub}_t2.nii.gz")
+            t1ce_path = os.path.join(sub_path, f"{sub}_t1ce.nii.gz")
+            flair_path = os.path.join(sub_path, f"{sub}_flair.nii.gz")
+            seg_path = os.path.join(sub_path, f"{sub}_seg.nii.gz")
+            
+            t1_img = read_nii(t1_path)
+            t2_img = read_nii(t2_path)
+            t1ce_img = read_nii(t1ce_path)
+            flair_img = read_nii(flair_path)
+            seg = read_nii(seg_path)
+            seg = seg.astype(np.int8)
+            
+            t1_img = zscore_nonzero(t1_img)
+            t2_img = zscore_nonzero(t2_img)
+            t1ce_img = zscore_nonzero(t1ce_img)
+            flair_img = zscore_nonzero(flair_img)
+            seg = convert_label(seg)
+            
+            data = np.stack([t1_img, t2_img, t1ce_img, flair_img, seg])  # 5*155*240*240
+            data = data[:, 5:145, 24:216, 24:216]  # crop to 5*140*192*192
+            
+            output_file = os.path.join(output_path, f"{sub}.npy")
+            np.save(output_file, data)
+            print(f"{sub} saved to {output_file}")
 
-        t1_img = zscore_nonzero(t1_img)
-        t2_img = zscore_nonzero(t2_img)
-        t1ce_img = zscore_nonzero(t1ce_img)
-        flair_img = zscore_nonzero(flair_img)
-        seg = convert_label(seg)
 
-        data = np.stack([t1_img, t2_img, t1ce_img, flair_img, seg])  # 5*155*240*240
-        data = data[:, 5:145, 24:216, 24:216]  # crop to 5*140*192*192
-        np.save(outputs + "%s.npy" % sub, data)
-        print("%s saved" % sub)
+if __name__ == "__main__":
+    main()
